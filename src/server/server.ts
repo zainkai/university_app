dcafdfa2c6dcf28c8fbc0890f7c05549ddf40201
Zainkai <turkingtonkevin@gmail.com>
2017-11-02 22:31:00 -0700

    working server

--- src/server/server.ts
+++ src/server/server.ts
@@ -8,11 +8,20 @@ const server = (http as any).Server(app);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use('/', express.static('./public'));
+app.engine('html', require('ejs').renderFile);
 
+
+app.get('/', (req, res, next) => {
+    res.render('index.html');
+});
 
 app.get('*', (req, res) => {
-    res.render('404page');
+    res.render('404page.html');
 });
+
+
 
 const port = process.env.PORT || 3000;
 server.listen(port);
+console.log("server running at http://localhost:"+port);

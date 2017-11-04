dfae9079458d8f2df92cd4565de9f5873d5ad4de
Zainkai <turkingtonkevin@gmail.com>
2017-11-03 21:16:41 -0700

    table rename

--- model/schema.sql
+++ model/schema.sql
@@ -1,40 +1,40 @@
-CREATE TABLE `fp_building` (
+CREATE TABLE `uni_building` (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
     `name` VARCHAR(255) NOT NULL, 
     `description` TEXT,
     CONSTRAINT UNIQUE (`name`)
 ) ENGINE=InnoDB;
 
-CREATE TABLE `fp_department` (
+CREATE TABLE `uni_department` (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
     `buildingid` INT NOT NULL,
     `name` VARCHAR(255) NOT NULL, 
     `description` TEXT,
-    FOREIGN KEY(`buildingid`) REFERENCES `fp_building`(`id`),
+    FOREIGN KEY(`buildingid`) REFERENCES `uni_building`(`id`),
     CONSTRAINT UNIQUE (`name`)
 ) ENGINE=InnoDB;
 
-CREATE TABLE `fp_class` (
+CREATE TABLE `uni_class` (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
     `name` VARCHAR(255) NOT NULL,
     `starttime` TIME,
     `endtime` TIME,
     `buildingid` INT NOT NULL,
     `departmentid` INT NOT NULL,
-    FOREIGN KEY(`buildingid`) REFERENCES `fp_building`(`id`),
-    FOREIGN KEY(`departmentid`) REFERENCES `fp_department`(`id`)
+    FOREIGN KEY(`buildingid`) REFERENCES `uni_building`(`id`),
+    FOREIGN KEY(`departmentid`) REFERENCES `uni_department`(`id`)
 ) ENGINE=InnoDB;
 
-CREATE TABLE `fp_student` (
+CREATE TABLE `uni_student` (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
     `name` VARCHAR(255) NOT NULL
 ) ENGINE=InnoDB;
 
-CREATE TABLE `fp_class_enrollment` (
+CREATE TABLE `uni_class_enrollment` (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
     `studentid` INT NOT NULL,
     `classid` INT NOT NULL,
-    FOREIGN KEY(`studentid`) REFERENCES `fp_student`(`id`),
-    FOREIGN KEY(`classid`) REFERENCES `fp_class`(`id`)
+    FOREIGN KEY(`studentid`) REFERENCES `uni_student`(`id`),
+    FOREIGN KEY(`classid`) REFERENCES `uni_class`(`id`)
 ) ENGINE=InnoDB;
 

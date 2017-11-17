5a9ee62b20dc4e5294eabf2c4f839b11b529b9cb
Zainkai <turkingtonkevin@gmail.com>
2017-11-17 11:17:30 -0800

    lol this worked

--- src/client/department/AddDepartmentModal.tsx
+++ src/client/department/AddDepartmentModal.tsx
@@ -9,9 +9,7 @@ interface Props {
     isVisible:boolean;
 };
 
-interface State {
-    newItem?: IDepartment;
-};
+interface State extends IDepartment{ };
 
 const addClient = (newItem:IDepartment) => post<Number>('/department/add', newItem);
 
@@ -28,9 +26,16 @@ export class AddDepartmentModal extends React.Component<Props,State>{
 
     }
 
+
+    //webpack:https://github.com/Mercateo/react-with-typescript/blob/master/webpack.config.js
+    inputHandler(e:React.FormEvent<HTMLInputElement>){
+        e.preventDefault();
+        this.setState({[e.currentTarget.name]: e.currentTarget.value} as any);
+    }
+
     render(){
         return(
-            <div className={"modal" + (this.props.isVisible ? " modal-show": " modal-hidden")}>
+            <div className={"modal" + (this.props.isVisible ? " modal-show" : " modal-hidden")}>
                 <div className="modal-header">
                     <h1>Add Department</h1>
                     <button onClick={this.toggleVisibility.bind(this)}>Close</button>
